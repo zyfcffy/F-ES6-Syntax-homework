ac2526ded5d126f0812e1f145bdfccac0c4290e1
fufangyuan <zyfcffy@163.com>
2020-11-10 00:10:57 +0800

    [fufangyuan]feat:finish

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (input) =>{
+    const {data, column} = input;
+    let result = [];
+    data.map(item => {
+        let obj = {};
+        item.forEach((i,index) => {
+            obj[column[index].name] = i;
+        })
+        result.push(obj);
+    });
+    return result
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) =>{
+    sections.sort((a,b) => b.index - a.index);
+    sections.forEach((i) =>{
+        items.splice(i.index, 0, i.content);
+    })
+    return items;
+}
 export { inject };
